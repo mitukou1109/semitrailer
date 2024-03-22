e6a9ad8a157f2f53ab6716124fa5536cb3e2a60b
mitukou1109 <mitukou1109@gmail.com>
2024-03-22 17:20:57 +0900

    add launch & config files

--- semitrailer_simulation/launch/bringup.launch.py
+++ semitrailer_simulation/launch/bringup.launch.py
@@ -1,7 +1,28 @@
-from launch import LaunchDescription
+import yaml
+from launch import LaunchContext, LaunchDescription
+from launch.actions import OpaqueFunction
 from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
+
+def _semitrailer_simulator_node(
+    context: LaunchContext,
+    semitrailer_props_yaml: PathJoinSubstitution,
+    semitrailer_simulator_config_yaml: PathJoinSubstitution,
+):
+    with open(context.perform_substitution(semitrailer_props_yaml), "r") as f:
+        semitrailer_props = yaml.safe_load(f)
+
+    return [
+        Node(
+            package="semitrailer_simulation",
+            executable="semitrailer_simulator_node",
+            name="semitrailer_simulator",
+            output="screen",
+            parameters=[semitrailer_props, semitrailer_simulator_config_yaml],
+        )
+    ]
 
 
 def generate_launch_description():
@@ -13,6 +34,12 @@ def generate_launch_description():
     )
     semitrailer_props_yaml = PathJoinSubstitution(
         [semitrailer_simulation_share_dir, "config", "semitrailer_40.yaml"]
+    )
+    joint_state_publisher_config_yaml = PathJoinSubstitution(
+        [semitrailer_simulation_share_dir, "config", "joint_state_publisher.yaml"]
+    )
+    semitrailer_simulator_config_yaml = PathJoinSubstitution(
+        [semitrailer_simulation_share_dir, "config", "semitrailer_simulator.yaml"]
     )
 
     robot_state_publisher_node = Node(
@@ -39,6 +66,12 @@ def generate_launch_description():
         executable="joint_state_publisher",
         name="joint_state_publisher",
         output="screen",
+        parameters=[joint_state_publisher_config_yaml],
+    )
+
+    semitrailer_simulator_node = OpaqueFunction(
+        function=_semitrailer_simulator_node,
+        args=[semitrailer_props_yaml, semitrailer_simulator_config_yaml],
     )
 
     rviz2_node = Node(
@@ -57,6 +90,7 @@ def generate_launch_description():
         [
             robot_state_publisher_node,
             joint_state_publisher_node,
+            semitrailer_simulator_node,
             rviz2_node,
         ]
     )
